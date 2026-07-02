201877e34926d7030a86b3042ab1276b35cbeb7c
AnshGupta-byte <anshg397397@gmail.com>
2026-07-02 11:56:52 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    int climbStairs(int n) {
+       
+       
+       /* if (n <= 2) return n;
+        vector<int> dp(n + 1, -1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+
+        */
+
+        if(n<=2)return n;
+        int a=1,b=2,c;
+        for(int i =3;i<=n;i++){
+            c = a+b;
+            a =b;
+            b=c;
+        }
+        return b;
+    }
+};
