9ea9314da212d588155e77e374e4864e25b94f78
AnshGupta-byte <anshg397397@gmail.com>
2026-07-06 23:19:02 +0530

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        bool x = false, y = false, z = false;
+        int n = triplets.size();
+        for (int i = 0; i < n; i++) {
+            if (triplets[i][0] > target[0] || triplets[i][1] > target[1] ||
+                triplets[i][2] > target[2]) {
+                continue;
+            }
+
+            if (triplets[i][0] == target[0]) x = true;
+            if (triplets[i][1] == target[1]) y = true;
+            if (triplets[i][2] == target[2]) z = true;
+        }
+        return x && y && z;
+    }
+};
